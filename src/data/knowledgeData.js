8e68f66f4b8bd2aa67057098c503ea4881e3971b
chalupa-bazooka <mteplyshev@gmail.com>
2024-04-14 15:02:26 +0400

    Change answers back to strings

--- src/data/knowledgeData.js
+++ src/data/knowledgeData.js
@@ -1,101 +1,109 @@
 export const knowledgeData = [
     {
         question: `What does Georgian Transliterator do?`,
-        answer: [
-            `It takes a Georgian text written in the Latin script and
-        turns it into a Georgian text written in the Georgian script.
-        So if you get a message with the word "gamarjoba"* you can
-        turn it into "გამარჯობა" with the help of this app.`,
-            `*"hello" in Georgian`,
-        ],
+        answer:
+            `It takes a Georgian text written in the Latin script and turns it
+        into a Georgian text written in the Georgian script. So if you get
+        a message with the word "gamarjoba"* you can turn it into "გამარჯობა"
+        with the help of this app.
+
+        *"hello" in Georgian`,
         open: false,
     },
     {
         question: `Does the transliterator translate Georgian to English?`,
-        answer: [
+        answer:
             `No, it doesn't. Translation and transliteration are 2 different
         things. However, they often work together and probably some day
-        there'll be a translator in this app too.`
-        ],
+        there'll be a translator in this app too.`,
         open: false,
     },
     {
         question: `Why would anyone need to transliterate?`,
-        answer: [
+        answer:
             `Sometimes you can come across a Georgian text written in
         Latin and have a hard time translating it to any other
         language. Most online translators can't translate a Georgian
         text in Latin scipt at all. So you have to first transliterate
-        the Georgian text in Latin script to the Georgian script.`
-        ],
+        the Georgian text in Latin script to the Georgian script.`,
         open: false,
     },
     {
         question: `What is the Georgian script?`,
-        answer: [
+        answer:
             `Unlike many other European languages, Georgian doesn't use
         the Latin alphabet. It uses an alphabet of its own. The Georgian
-        script is basically the same as the Georgian alphabet.`
-        ],
+        script is basically the same as the Georgian alphabet.`,
         open: false,
     },
     {
         question: `Where can I come across Georgian text written in Latin?`,
-        answer: [
+        answer:
             `The most likely source of such texts are mobile messages. If you
         stay in Georgia and have a Georgian mobile number, you likely
         receive advertisement meobile messages from time to time. If you
         use Georgian services (banks, parking, etc), you'll also likely
         receive mobile messages from them. More often than not, those
         messages are written in Georgian but in the Latin script. So to
-        understand what the message is about, you'll need a transliterator.`,
-            `Also, you can stumble upon Georgian written in Latin on the web.
-        Some Facebook users write that way sometimes out of their preference.`,
-            `And if you've just started studying Georgian yourself, you might find it
+        understand what the message is about, you'll need a transliterator.
+
+        Also, you can stumble upon Georgian written in Latin on the web.
+        Some Facebook users write that way sometimes out of their preference.
+
+        And if you've just started studying Georgian yourself, you might find it
         easier to stick to the Latin alphabet before you're confident with
         Georgian letters. The transliterator might be of use to you in such case too.
         `,
-        ],
         open: false,
     },
     {
         question: `Why can some Latin letters transliterate
         as more than one Georgian letter?`,
-        answer: [
-            `While the Georgian alphabet looks nothing like the Latin one, it's
+        answer: `While the Georgian alphabet looks nothing like the Latin one, it's
             good news that most its letters represent the same sounds that Latin
             letters do. Some sounds, however, can be written down as several letters.
             Consider the sound "k". In how many ways can you right it down in English?
             "King" has it as "k", "cat" has it as "c", and "quay" has it as "q".
-            Georgian has similar situations of its own.`,
-            `Here's the list of all such cases in Georgian:`,
-            `• c – "ts" as in "ca<b>ts</b>" – can be written both as ც and წ;`,
-            `• ch – "ch" as in "<b>ch</b>air" – can be written both as ჩ and ჭ;`,
-            `• g – can represent 2 different sounds "g" as in "<b>g</b>et" and an
+            Georgian has similar situations of its own.
+
+            Here's the list of all such cases in Georgian:
+
+            • c – "ts" as in "cats" – can be written both as ც and წ;
+
+            • ch – "ch" as in "chair" – can be written both as ჩ and ჭ;
+
+            • g – can represent 2 different sounds "g" as in "get" and an
             exotic sound not met in English that is something between "g" and "h" –
-            the former is written as გ and the latter is written as ღ`,
-            `• h – "h" as in "<b>h</b>elp" or "x" as in the Scottish "loch"
-            or the Spanish "Mé<b>x</b>ico" – can be written as both ჰ and ხ;`,
-            `• j – can represent 2 different sounds: "dj" as in "<b>J</b>oe" and
-            "j" as in "mea<b>su</b>re" – the former is ჯ in Georgian and the
-            latter is ჟ;`,
-            `• k – "k" as in "<b>k</b>ing" – can be written as კ, ქ, and ყ.
+            the former is written as გ and the latter is written as ღ
+
+            • h – "h" as in "help" or "x" as in the Scottish "loch"
+            or the Spanish "México" – can be written as both ჰ and ხ;
+
+            • j – can represent 2 different sounds: "dj" as in "Joe" and
+            "j" as in "measure" – the former is ჯ in Georgian and the
+            latter is ჟ;
+
+            • k – "k" as in "king" – can be written as კ, ქ, and ყ.
             Technically, all those letters represent different sounds (especially
             ყ), but that's just how those letters are usually transliterated –
-            as k;`,
-            `• p – "p" as in "<b>p</b>ot" – can be written as პ and ფ;`,
-            `• t – "t" as in "<b>t</b>ornado" – can be written as თ and ტ.`,
-            `There are several more cases when our Transliterator offers alternative options:`,
-            `• ts – can be transliterated as a single letter (ც or წ) or a combination of 2
+            as k;
+
+            • p – "p" as in "pot" – can be written as პ and ფ;
+
+            • t – "t" as in "tornado" – can be written as თ and ტ.
+
+            There are several more cases when our Transliterator offers alternative options:
+
+            • ts – can be transliterated as a single letter (ც or წ) or a combination of 2
             letters (თს or ტს). They literally sound the same similarly to how the English
-            letter "x" sounds the same as the combination of letters "ks";`,
-            `• y – it's a very tricky letter to translitearte. Most of the time it's transliterated
+            letter "x" sounds the same as the combination of letters "ks";
+
+            • y – it's a very tricky letter to translitearte. Most of the time it's transliterated
             as ყ. The only reason that is so, is because they look similar. But they stand for
             very different sounds. But traditionally the Georgian ყ is transliterated as "y" in
             the Latin script. However, if you want to transliterate a proper noun like "Yellowstone",
             the letter you'll want to use for "Y" at the beginning is "ი". We offer it as an alterantive
             option whenever there's the letter "y" in the input.`,
-        ],
         open: false,
     }
 
