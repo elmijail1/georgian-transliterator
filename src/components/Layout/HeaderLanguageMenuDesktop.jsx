afe3c9e5233a9e9d77d8213e75a16b539b3b52f1
chalupa-bazooka <mteplyshev@gmail.com>
2024-04-17 20:43:11 +0400

    Start working on HeaderLanguageMenuDesktop

--- src/components/Layout/HeaderLanguageMenuDesktop.jsx
+++ src/components/Layout/HeaderLanguageMenuDesktop.jsx
@@ -1,5 +1,72 @@
+import { useContext } from "react"
+import { LayoutContext } from "./Layout.jsx"
+
 export default function HeaderLanguageMenuDesktop() {
+    const {
+        language,
+        languageMenuOpen,
+        setLanguage,
+        setLanguageMenuOpen,
+        setMenuOpen
+    } = useContext(LayoutContext)
+
     return (
-        <h1>languages</h1>
+        <div className="Header__Languages">
+            <button
+                className="Header__Languagues--Button"
+                onClick={() => {
+                    setLanguageMenuOpen(prevOpen => !prevOpen)
+                    setMenuOpen(false)
+                }}
+            >
+                <span>
+                {
+                    language === "RUS"
+                    ? "Русский"
+                    : "Английский"
+                }
+                </span>
+                <span>
+                {
+                    languageMenuOpen
+                    ? "▼"
+                    : "▲"
+                }
+                </span>
+            </button>
+            {
+                languageMenuOpen &&
+                <ul className="Header__Languages--Menu">
+                    <li
+                        className={language === "ENG" ? "Header__Languages--MenuItemActive" : ""}
+                        onClick={() => {
+                            setLanguage("ENG")
+                            setLanguageMenuOpen(false)
+                            localStorage.setItem("language", "ENG")
+                        }}
+                    >
+                        {
+                            language === "RUS"
+                                ? "Английский"
+                                : "English"
+                        }
+                    </li>
+                    <li
+                        className={language === "RUS" ? "Header__Languages--MenuItemActive" : ""}
+                        onClick={() => {
+                            setLanguage("RUS")
+                            setLanguageMenuOpen(false)
+                            localStorage.setItem("language", "RUS")
+                        }}
+                    >
+                        {
+                            language === "RUS"
+                                ? "Русский"
+                                : "Russian"
+                        }
+                    </li>
+                </ul>
+            }
+        </div>
     )
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,9 +14,9 @@ export const LayoutContext = createContext()
 export default function Layout() {
     const [menuOpen, setMenuOpen] = useState(false) // this state controls the kebab menu, whether it's opened or not
     const [languageMenuOpen, setLanguageMenuOpen] = useState(false) // this state controls the language menu, whether it's opened or not
-    const [language, setLanguage] = useState()
+    const [language, setLanguage] = useState() // this state controls the UI's language
 
-    // this effect saves lanuage preferences in the local storage
+    // this effect saves language preferences in the local storage
     useEffect(() => {
         if (localStorage.getItem("language")) {
             setLanguage(localStorage.getItem("language"))
@@ -25,7 +25,8 @@ export default function Layout() {
         }
     }, [])
 
-    const [vpWidth, setVPWidth] = useState(window.innerWidth)
+    const [vpWidth, setVPWidth] = useState(window.innerWidth) // this state controls width of the viewport
+    // this effect updates the vpWidth state whenever the viewport's width changes
     useEffect(() => {
         function handleResize() {
             setVPWidth(window.innerWidth)
@@ -33,7 +34,8 @@ export default function Layout() {
         window.addEventListener("resize", handleResize)
     })
 
-    const layoutContext = {
+    // these are states & functions shared with components via LayoutContext
+    const layoutContextContents = {
         language,
         languageMenuOpen,
         menuOpen,
@@ -45,7 +47,7 @@ export default function Layout() {
     return (
         <>
             <div className="Header">
-                <LayoutContext.Provider value={layoutContext}>
+                <LayoutContext.Provider value={layoutContextContents}>
                     <HeaderLetteringAndLogo />
                     {/* <div>vpWidth = {vpWidth}</div> */}
                     {
