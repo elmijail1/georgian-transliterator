183094a414b29758890f58ce377218dcf83382cc
chalupa-bazooka <mteplyshev@gmail.com>
2024-04-24 22:04:24 +0400

    Add mobile styles to Contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,85 +5,88 @@ export default function Contacts() {
     const language = useOutletContext()
 
     return (
-        <main className="Contacts__Main">
-            <h1 className="Contacts__Header">
-                {
-                    language === "RUS"
-                        ? "Контакты"
-                        : "Contacts"
-                }
-            </h1>
-            <p>
-                {
-                    language === "RUS"
-                        ? "Вы можете связаться с нами любым из этих способов. Будем рады получить обратную связь и ответить на ваши вопросы!"
-                        : "Feel free to contact us for any questions and suggestions!"
-                }
-            </p>
+        <main>
+            <div className="Contacts__Main">
 
-            <div className="Contacts__Box">
+                <h1 className="Contacts__Header">
+                    {
+                        language === "RUS"
+                            ? "Контакты"
+                            : "Contacts"
+                    }
+                </h1>
+                <p>
+                    {
+                        language === "RUS"
+                            ? "Вы можете связаться с нами любым из этих способов. Будем рады получить обратную связь и ответить на ваши вопросы!"
+                            : "Feel free to contact us for any questions and suggestions!"
+                    }
+                </p>
 
-                <div
-                    className="Contacts__General"
-                    onClick={() => navigator.clipboard.writeText("elmijail1@proton.me")}
-                >
-                    <div className="Contacts__BoxImageChannel">
-                        <p className="Contacts__Image">✉️</p>
-                        <p className="Contacts__Channel">
+                <div className="Contacts__Box">
+
+                    <div
+                        className="Contacts__General"
+                        onClick={() => navigator.clipboard.writeText("elmijail1@proton.me")}
+                    >
+                        <div className="Contacts__BoxImageChannel">
+                            <p className="Contacts__Image">✉️</p>
+                            <p className="Contacts__Channel">
+                                {
+                                    language === "RUS"
+                                        ? "Эл. почта"
+                                        : "Email"
+                                }
+                            </p>
+                        </div>
+                        <p className="Contacts__Contact">elmijail1@proton.me</p>
+                        <p className="Contacts__Copy">
                             {
                                 language === "RUS"
-                                    ? "Эл. почта"
-                                    : "Email"
+                                    ? "Скопировать"
+                                    : "Click to copy"
                             }
                         </p>
                     </div>
-                    <p className="Contacts__Contact">elmijail1@proton.me</p>
-                    <p className="Contacts__Copy">
-                        {
-                            language === "RUS"
-                                ? "Скопировать"
-                                : "Click to copy"
-                        }
-                    </p>
-                </div>
 
-                <div
-                    className="Contacts__General"
-                    onClick={() => navigator.clipboard.writeText("@elmijail1")}
+                    <div
+                        className="Contacts__General"
+                        onClick={() => navigator.clipboard.writeText("@elmijail1")}
 
-                >
-                    <div className="Contacts__BoxImageChannel">
-                        <p className="Contacts__Image">✈️</p>
-                        <p className="Contacts__Channel">Telegram</p>
+                    >
+                        <div className="Contacts__BoxImageChannel">
+                            <p className="Contacts__Image">💬</p>
+                            <p className="Contacts__Channel">Telegram</p>
+                        </div>
+                        <p className="Contacts__Contact">@elmijail1</p>
+                        <p className="Contacts__Copy">
+                            {
+                                language === "RUS"
+                                    ? "Скопировать"
+                                    : "Click to copy"
+                            }
+                        </p>
                     </div>
-                    <p className="Contacts__Contact">@elmijail1</p>
-                    <p className="Contacts__Copy">
-                        {
-                            language === "RUS"
-                                ? "Скопировать"
-                                : "Click to copy"
-                        }
-                    </p>
-                </div>
-                
-                <div
-                    className="Contacts__General"
-                    onClick={() => navigator.clipboard.writeText("elmijail1")}
-                >
-                    <div className="Contacts__BoxImageChannel">
-                        <p className="Contacts__Image">👾</p>
-                        <p className="Contacts__Channel">Discord</p>
-                    </div>
-                    <p className="Contacts__Contact">elmijail1</p>
-                    <p className="Contacts__Copy">
-                        {
-                            language === "RUS"
-                                ? "Скопировать"
-                                : "Click to copy"
-                        }
-                    </p>
-                </div>
 
+                    <div
+                        className="Contacts__General"
+                        onClick={() => navigator.clipboard.writeText("elmijail1")}
+                    >
+                        <div className="Contacts__BoxImageChannel">
+                            <p className="Contacts__Image">👾</p>
+                            <p className="Contacts__Channel">Discord</p>
+                        </div>
+                        <p className="Contacts__Contact">elmijail1</p>
+                        <p className="Contacts__Copy">
+                            {
+                                language === "RUS"
+                                    ? "Скопировать"
+                                    : "Click to copy"
+                            }
+                        </p>
+                    </div>
+
+                </div>
             </div>
 
         </main>
