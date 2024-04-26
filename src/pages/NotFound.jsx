746df7a916780160d21bda025b074778b3e82833
chalupa-bazooka <mteplyshev@gmail.com>
2024-04-26 22:18:58 +0400

    Add mobile styles to NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,58 @@
+import { Link, useOutletContext } from "react-router-dom"
+
 export default function NotFound() {
+
+    const language = useOutletContext()
+
     return (
-        <h1>NOT FOUND</h1>
+        <main>
+            <div className="NotFound__MainDiv">
+                <h1 className="NotFound__Header">
+
+                    {
+                        language === "RUS"
+                            ? "Такой страницы у нас пока нет :-/"
+                            : "No such page :-/"
+                    }
+                </h1>
+                <p className="NotFound__Description">
+                    {
+                        language === "RUS"
+                            ? "Но есть такие:"
+                            : "Try these pages:"
+                    }
+                </p>
+                <ul className="NotFound__Links">
+                    <li>
+                        <Link to="/" >
+                            {
+                                language === "RUS"
+                                    ? "Транслитератор"
+                                    : "Transliterator"
+                            }
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/knowledge">
+                            {
+                                language === "RUS"
+                                    ? "Полезно знать"
+                                    : "Knowledge"
+                            }
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contacts">
+                            {
+                                language === "RUS"
+                                    ? "Контакты"
+                                    : "Contacts"
+                            }
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        // add navigation + styling
     )
 }
