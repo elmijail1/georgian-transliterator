17677351d93f183fe86caeb62a2b200b05ea589c
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-11 00:35:54 +0400

    Update the additional options styles for mobile
    - tooltip > regular text
    - toggler switch > full-width button
    - letters option menu is now displayed on the right, not under the highlighted letter

--- src/components/AlternativeOptionsDesktop.jsx
+++ src/components/AlternativeOptionsDesktop.jsx
@@ -1,6 +1,9 @@
 import { useContext } from "react"
-import { Link } from "react-router-dom"
+import { Link, useOutletContext } from "react-router-dom"
 import { TransliteratorContext } from "../pages/Transliterator.jsx"
+
+import { MdLightbulbOutline, MdLightbulb } from "react-icons/md";
+
 
 export default function AlternativeOptionsDesktop() {
 
@@ -10,6 +13,44 @@ export default function AlternativeOptionsDesktop() {
         setAlternativeOptions,
         setOptionsDisplay,
     } = useContext(TransliteratorContext)
+
+    const { vpWidth } = useOutletContext()
+
+    if (vpWidth < 450) {
+        return (
+            <div
+                className="aoMobile__General"
+            >
+                <button className="aoMobile__Button">
+                    <MdLightbulbOutline className="aoMobile__ButtonBulb" />
+                    <div className="aoMobile__ButtonText">
+                        {
+                            language === "RUS"
+                                ? `${optionsDisplay ? "Спрятать" : "Показать"} альтернативные опции перевода букв`
+                                : `${optionsDisplay ? "Hide" : "Show"} alternative options for letters`
+                        }
+                    </div>
+                </button>
+                <div className="aoMobile__Description">
+                    {
+                        language === "RUS"
+                            ? "Нажмите на подсвеченную букву, чтобы узнать, как ещё её можно перевести. Узнайте о том, почему одну букву можно перевести по-разному, "
+                            : "Press a highlighted letter to see how else it can be transliterated. Learn more about why it's important "
+                    }
+                    <Link
+                        to="/knowledge"
+                        className="ExtraTools__Info--Link"
+                    >
+                        {
+                            language === "RUS"
+                                ? "здесь"
+                                : "here"
+                        }
+                    </Link>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div
@@ -24,7 +65,7 @@ export default function AlternativeOptionsDesktop() {
                     setAlternativeOptions(({ shown: false, geoChar: "", latInit: "", index: null }))
                 }}
             />
-            
+
             <label htmlFor="aoDesktop__Checkbox" id="aoDesktop__SwitchLabel"></label>
 
             <div className="aoDesktop__RightBox">
