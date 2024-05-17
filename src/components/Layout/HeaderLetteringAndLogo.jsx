fed92fab1d93232ebaa5e24c484c852e6e99968a
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-17 15:44:07 +0400

    Add a proper image for a logo

--- src/components/Layout/HeaderLetteringAndLogo.jsx
+++ src/components/Layout/HeaderLetteringAndLogo.jsx
@@ -10,12 +10,13 @@ export default function HeaderLetteringAndLogo() {
             }}
         >
             <div className="Header__HomeDiv">
-                <div className="Header__Logo">
+                {/* <div className="Header__Logo">
                     <span className="Header__Logo--tlat">t</span>
 
                     <span className="Header__Logo--slash">⃕</span>
                     <span className="Header__Logo--tgeo">თ</span>
-                </div>
+                </div> */}
+                <img src="transliterator-logo.png" className="Header__LogoImage" alt="" />
                 <div className="Header__Title">Georgian Transliterator </div>
             </div>
         </Link>
