c1a77b029b04da1a0a212bfb664bf004658fabdb
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-24 11:02:53 +0400

    Move NotFound buttons' data to a separate js file

---
+++ src/data/notFoundData.js
@@ -0,0 +1,17 @@
+export const notFoundData = [
+    {
+        textEng: "Transliterator",
+        textRus: "Транслитератор",
+        path: "/",
+    },
+    {
+        textEng: "Knowledge",
+        textRus: "Полезно знать",
+        path: "/knowledge",
+    },
+    {
+        textEng: "Contacts",
+        textRus: "Контакты",
+        path: "/contacts",
+    },
+]
