f6d292554fc5d6f1fce263fe137787f54e33f8d6
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-24 11:19:28 +0400

    Add consistency & clarity
    - add section names
    - change class names to match the convention
    - move buttons' layout to a separate component file

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,70 +1,48 @@
+// general
 import { useOutletContext } from "react-router-dom"
 import { nanoid } from "nanoid"
-
+// data
 import { contactsData } from "../data/contactsData.js"
+// components
+import ContactsButton from "../components/Contacts/ContactsButton.jsx"
+
 
 export default function Contacts() {
-
-    const {language} = useOutletContext()
+    const { language } = useOutletContext() // *1
 
     return (
         <main>
-            <div className="Contacts__Main">
 
-                <h1 className="Contacts__Header">
+            <div className="Contacts__GeneralDiv">
+
+                {/* title */}
+                <h1 className="Contacts__Title">
                     {
-                        language === "RUS"
-                            ? "Контакты"
-                            : "Contacts"
+                        language === "ENG"
+                            ? "Contacts"
+                            : "Контакты"
                     }
                 </h1>
+
+                {/* description */}
                 <p className="Contacts__Description">
                     {
-                        language === "RUS"
-                            ? "Вы можете связаться с нами любым из этих способов. Будем рады получить обратную связь и ответить на ваши вопросы!"
-                            : "Feel free to contact us for any questions and suggestions!"
+                        language === "ENG"
+                            ? "Feel free to contact us for any questions and suggestions!"
+                            : "Вы можете связаться с нами любым из этих способов. Будем рады получить обратную связь и ответить на ваши вопросы!"
                     }
                 </p>
 
-                <div className="Contacts__Box">
+                {/* buttons */}
+                <div className="Contacts__Buttons">
                     {
-                        contactsData.map(contact => {
+                        contactsData.map(entry => {
                             return (
-                                <div
-                                    className="Contacts__General"
-                                    onClick={() => navigator.clipboard.writeText(contact.address)}
+                                <ContactsButton
+                                    entry={entry}
+                                    language={language}
                                     key={nanoid()}
-                                >
-                                    <div className="Contacts__TextHalf">
-                                        <p
-                                            className="Contacts__TextHalf__Name"
-                                        >
-                                            {contact.channel}
-                                        </p>
-                                        <p
-                                            className="Contacts__TextHalf__Contact"
-                                        >
-                                            {contact.address}
-                                        </p>
-                                        <p
-                                            className="Contact__TextHalf__CopyButton"
-                                        >
-                                            {
-                                                language === "RUS"
-                                                    ? "Скопировать"
-                                                    : "Click to copy"
-                                            }
-                                        </p>
-                                    </div>
-
-                                    <div>
-                                        <img
-                                            src={contact.image}
-                                            className="Contacts__ImageHalf__Image"
-                                            alt={contact.image}
-                                        />
-                                    </div>
-                                </div>
+                                />
                             )
                         })
                     }
@@ -76,3 +54,16 @@ export default function Contacts() {
         </main>
     )
 }
+
+{/*
+BASIC RENDER STRUCTURE
+[0. Header (handled by the Layout page)]
+1. Title
+2. Description
+3. Buttons
+
+COMMENTS
+- 1*. "language" defines the language of the UI (as chosen by user)
+and comes from the outlet context set in the Layout page.
+
+*/}
