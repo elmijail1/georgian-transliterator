2dc272072b6449853336ce8d6a4ad44cd8383b0f
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-25 11:58:23 +0400

    Add consistency & clarity
    - change the class name
    - add comments & render structure description

--- src/components/Knowledge/KnowledgeItemAnswer.jsx
+++ src/components/Knowledge/KnowledgeItemAnswer.jsx
@@ -1,9 +1,9 @@
+// utilities
 import { splitAnswerIntoDivsAndMap } from "../../utilities/knowledgeUtilities"
 
 export default function KnowledgeItemAnswer({ entry, language }) {
-
     return (
-        <div className="Knowledge__SingleItem--Answer">
+        <div className="KnowAnswer__GeneralDiv">
             {language === "ENG"
                 ? splitAnswerIntoDivsAndMap(entry.answer)
                 : splitAnswerIntoDivsAndMap(entry.answerRus)
@@ -11,3 +11,16 @@ export default function KnowledgeItemAnswer({ entry, language }) {
         </div>
     )
 }
+
+/*
+RENDER STRUCTURE:
+1. Description (split in lines at every \n\n
+
+COMMENT
+Accepted values:
+- entry: it comes from knowledgeItems and represents a question-answer pair.
+There are answers for both English & Russian versions of the UI.
+- language: the language of the interface set by the user. It comes from the
+Layout through a context.
+)
+*/
