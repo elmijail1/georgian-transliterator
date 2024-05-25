33d59f1e0a82f8813ed6f2e5d3403e0474c8dff2
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-25 19:50:48 +0400

    Move the state setting of knowledgeItems back to the Knowledge page

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,8 @@ import Layout from "./pages/Layout.jsx"
 export const KnowledgeContext = createContext()
 
 export default function App() {
-
-  // page-Knowledge data 1*
-  const [knowledgeItems, setKnowledgeItems] = useState(knowledgeData)
-  const knowledgeContextContents = {
-    knowledgeItems,
-    setKnowledgeItems
-  }
-
   return (
     <BrowserRouter>
-      <KnowledgeContext.Provider value={knowledgeContextContents}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Transliterator />} />
@@ -38,18 +29,6 @@ export default function App() {
             <Route path="knowledge-experiment" element={<KnowledgeExperiment />} />
           </Route>
         </Routes>
-      </KnowledgeContext.Provider>
     </BrowserRouter>
   )
 }
-
-{/*
-COMMENTS
-1*. page-Knowledge data
-We need to create this state here and pass it to the context to achieve
-just 1 thing: being able to click a link on Transliterate page and go
-to the Knowledge page's section (by #id) and change the state of its
-items. The 2 pages must share the state for that and App.jsx is their
-first common relative.
-
-*/}
