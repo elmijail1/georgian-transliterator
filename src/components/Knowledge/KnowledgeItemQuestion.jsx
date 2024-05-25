8c4af9d5a86c2b2ae22d3afdb83b9fb161426f07
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-25 12:24:40 +0400

    Add consistency & clarity
    - clean up class names
    - clean up the code
    - add comments & render structure

--- src/components/Knowledge/KnowledgeItemQuestion.jsx
+++ src/components/Knowledge/KnowledgeItemQuestion.jsx
@@ -1,26 +1,44 @@
+// utilities
 import { showAnswer } from "../../utilities/knowledgeUtilities"
 
 export default function KnowledgeItemQuestion({ entry, index, language, setKnowledgeItems }) {
-
     return (
-        <div
+        <button
             onClick={() => showAnswer(index, setKnowledgeItems)}
-            className={`Knowledge__SingleItem--QuestionButton ${entry.open && "Knowledge__SingleItem--QuestionButtonOpen"}`}>
-            <button
-                className="Knowledge__SingleItem--QuestionButton--Question"
-            >
+            className={`KnowQuestion__Button ${entry.open && "KnowQuestion__ButtonOpen"}`}>
+
+            {/* question text */}
+            <div className="KnowQuestion__Text">
                 {
                     language === "ENG"
                         ? entry.question
                         : entry.questionRus
                 }
-            </button>
-            {
-                entry.open
-                    ? <div className="Knowledge__SingleItem--QuestionButton--Button">▲</div>
-                    : <div className="Knowledge__SingleItem--QuestionButton--Button">▼</div>
-            }
+            </div>
 
-        </div>
+            {/* open / close symbol */}
+            <div className="KnowQuestion__OpenCloseSymbol">
+                {entry.open ? "▲" : "▼"}
+            </div>
+
+        </button>
     )
 }
+
+{/*
+RENDER STRUCTURE
+1. Button
+1.1. Question text
+1.2. Open / close symbol
+
+COMMENTS
+1. What this component is
+It renders the text from the "question" property of the knowledgeItems array's entry.
+Also it can be clicked to show / hide the respective answer.
+.
+2. Values it accepts
+- entry: an object from the knowledgeItems array containing a question-answer pair;
+- index: an index of that same entry;
+- language: a user-set language controlled by a state in the Layout page component;
+- setKnowledgeItems: the knowledgeItems' state setter from the Knowledge page;
+*/}
