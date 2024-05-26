27513edd92abb22ee698d3469665f4120bfe10cf
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-26 18:03:32 +0400

    Add clarity & consistency
    - move closeMenuIfClickedElsewhere to layoutUtilities
    - add comments

--- src/components/Layout/MenuMobile.jsx
+++ src/components/Layout/MenuMobile.jsx
@@ -5,6 +5,8 @@ import { useContext, useRef, useEffect } from "react"
 import MenuItem from "./MenuItem.jsx"
 // data
 import { menuData } from "../../data/layoutData.js"
+// utilities
+import { closeMenuIfClickedElsewhere } from "../../utilities/layoutUtilities.js"
 // context
 import { LayoutContext } from "../../pages/Layout.jsx"
 // svg
@@ -12,7 +14,7 @@ import { MdMenu } from "react-icons/md";
 
 
 export default function MenuMobile() {
-    const {
+    const { // Context 1*
         language,
         menuOpen,
         setLanguageMenuOpen,
@@ -20,21 +22,17 @@ export default function MenuMobile() {
     } = useContext(LayoutContext)
 
 
-    let menuRef = useRef() // these 2 are used to make the kebab menu close when clicked elsewhere; ref refers to the menu element in the render
-    useEffect(() => { // adds logic for closing the languageMenu whenever clicked elsewhere
-        function closeMenuIfClickedElsewhere(event) {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
-                setMenuOpen(false)
-            }
-        }
-
-        document.addEventListener("mousedown", closeMenuIfClickedElsewhere)
-
+    let menuRef = useRef() // Elsewhere clicks handling 2.1*
+    useEffect(() => {   // Elsewhere clicks handling 2.2*
+        // Listener attachment 2.2.i*
+        document.addEventListener("mousedown", () => closeMenuIfClickedElsewhere(event, menuRef, setMenuOpen))
+        // Cleanup 2.2.ii*
         return (() => {
             document.removeEventListener("mousedown", closeMenuIfClickedElsewhere)
         })
     })
 
+    // Active styles 3*
     const activeStyles = { fontWeight: 700 }
 
     return (
@@ -72,3 +70,30 @@ export default function MenuMobile() {
         </div>
     )
 }
+
+{/*
+
+COMMENTS
+1. Context
+Several states & setters are imported through the LayoutContext from the Layout page.
+- language: determines the UI's language as set by the user;
+- menuOpen & setMenuOpen: determine if this component is shown;
+- setLanguageMenuOpen: determines if the Language component is shown. It's necessary
+to hide it whenever this (MenuMobile) component is shown.
+.
+2. Elsewhere clicks handling
+If the user clicks somewhere other than this component when it's shown, the component
+should be hidden right away.
+2.1. menuRef is a reference that we set to the GeneralDiv of this component.
+2.2. This effect contains the elsewhere clicks handling logic. The function that's
+used here is closeMenuIfClickedElsewere and it's defined & explained in the utility
+file "utilities/layoutUtilities".
+Event's stages:
+- i. Listener attachment. It's attached right to the document object. The trigger event
+is "mousedown".
+- ii. Cleanup. At the end of the effect's cycle the listener is removed.
+.
+3. Active styles
+It's an object containing additional styles applied to a currently active NavLink
+in the MenuItem component mapped with menuData.
+*/}
