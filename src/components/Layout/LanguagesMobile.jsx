59bae903b355f1c57706d3fc0d9e99e794b8f1b4
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-26 19:41:52 +0400

    Add calrity & consistency
    - add comments
    - move LanguagesItem to a separate component

--- src/components/Layout/LanguagesMobile.jsx
+++ src/components/Layout/LanguagesMobile.jsx
@@ -1,5 +1,8 @@
 // general
 import { useContext, useRef, useEffect } from "react"
+import { nanoid } from "nanoid"
+// components
+import LanguagesItem from "./LanguagesItem.jsx"
 // data
 import { languagesData } from "../../data/layoutData.js"
 // utilities
@@ -9,7 +12,7 @@ import { LayoutContext } from "../../pages/Layout.jsx"
 
 export default function LanguagesMobile() {
 
-    const {
+    const {     // Context 1*
         language,
         languageMenuOpen,
         setLanguage,
@@ -18,26 +21,35 @@ export default function LanguagesMobile() {
     } = useContext(LayoutContext)
 
 
-    let languageMenuRef = useRef()
-    useEffect(() => {
+    let languageMenuRef = useRef() // Elsewhere clicks handling 2.1*
+    useEffect(() => {      // Elsewhere clicks handling 2.2*
+        // Listener attachment 2.2.i*
         document.addEventListener(
             "mousedown",
             () => closeMenuIfClickedElsewhere(
                 event, languageMenuRef, setLanguageMenuOpen
             )
         )
-
+        // Cleanup 2.2.ii*
         return (() => {
             document.removeEventListener("mousedown", closeMenuIfClickedElsewhere)
         })
     })
 
+    function chooseLanguage(entry) {
+        setLanguage(entry.langShort)
+        setLanguageMenuOpen(false)
+        localStorage.setItem("language", entry.langShort)
+    }
+
 
     return (
-        <div className="Header__Languages" ref={languageMenuRef}>
+        // container div
+        <div className="Lang__GeneralDiv" ref={languageMenuRef}>
 
+            {/* button */}
             <button
-                className="Header__Languagues--Button"
+                className="Lang__Button"
                 onClick={() => {
                     setLanguageMenuOpen(prevOpen => !prevOpen)
                     setMenuOpen(false)
@@ -46,24 +58,19 @@ export default function LanguagesMobile() {
                 {language === "ENG" ? "ENG" : "RUS"}
             </button>
 
+            {/* list */}
             {languageMenuOpen &&
-                <ul className="Header__Languages--Menu">
+                <ul className="Lang__Menu">
 
                     {
                         languagesData.map(entry => (
-                            <li
-                                className={language === entry.langShort ? "Header__Languages--MenuItemActive" : ""}
-                                onClick={() => {
-                                    setLanguage(entry.langShort)
-                                    setLanguageMenuOpen(false)
-                                    localStorage.setItem("language", entry.langShort)
-                                }}
-                            >
-                                {language === entry.langShort
-                                    ? entry.langFullNative
-                                    : entry.langFullTrans
-                                }
-                            </li>
+                            // list item
+                            <LanguagesItem
+                                entry={entry}
+                                language={language}
+                                chooseLanguage={chooseLanguage}
+                                key={nanoid()}
+                            />
                         ))
                     }
 
@@ -73,3 +80,31 @@ export default function LanguagesMobile() {
         </div>
     )
 }
+
+{/*
+RENDER STRUCTURE
+1. Component div
+1.1. Button (determines if the List (1.2) is shown or hidden)
+1.2. List
+1.2.1. List item
+
+COMMENTS
+1. Context
+Several states & setters are imported through the LayoutContext from the Layout page.
+- language & setLanguage: determines the UI's language as set by the user;
+- languageMenuOpen & setLanguageMenuOpen: determine if this component is shown;
+- setMenuOpen: determines if the Menu (Mobile or Desktop) component is shown. It's
+necessary to hide it whenever this (Languages) component is shown.
+.
+2. Elsewhere clicks handling
+If the user clicks somewhere other than this component when it's shown, the component
+should be hidden right away.
+2.1. menuRef is a reference that we set to the GeneralDiv of this component.
+2.2. This effect contains the elsewhere clicks handling logic. The function that's
+used here is closeMenuIfClickedElsewere and it's defined & explained in the utility
+file "utilities/layoutUtilities".
+Event's stages:
+- i. Listener attachment. It's attached right to the document object. The trigger event
+is "mousedown".
+- ii. Cleanup. At the end of the effect's cycle the listener is removed.
+*/}
