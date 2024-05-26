c5e768065a505a33397bc33c0dbf4c2441d2bebe
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-26 13:48:30 +0400

    Add consistency & clarity
    - add comments & render structure
    - change class names

--- src/components/Layout/LetteringAndLogo.jsx
+++ src/components/Layout/LetteringAndLogo.jsx
@@ -1,24 +1,31 @@
+// general
 import { Link } from "react-router-dom"
 
 export default function LetteringAndLogo() {
     return (
-        <Link
-            to="/"
-            className="Header__HomeLink"
-            style={{
-                textDecoration: "none"
-            }}
-        >
-            <div className="Header__HomeDiv">
-                {/* <div className="Header__Logo">
-                    <span className="Header__Logo--tlat">t</span>
+        // link + container
+        <Link to="/" className="LAL__Link">
+            <div className="LAL__GeneralDiv">
 
-                    <span className="Header__Logo--slash">⃕</span>
-                    <span className="Header__Logo--tgeo">თ</span>
-                </div> */}
-                <img src="transliterator-logo.png" className="Header__LogoImage" alt="" />
-                <div className="Header__Title">Georgian Transliterator </div>
+                {/* image */}
+                <img
+                    className="LAL__Image"
+                    src="transliterator-logo.png"
+                    alt="georgian transliterator logo"
+                />
+
+                {/* title */}
+                <div className="LAL__Title">Georgian Transliterator</div>
+
             </div>
         </Link>
+
     )
 }
+
+{/*
+RENDER STRUCTURE:
+1. Link + Container
+1.1. Image
+1.2. Title
+*/}
