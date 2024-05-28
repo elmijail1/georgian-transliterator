7c376948fa5ebef83f1e2fc238d9036e9004a6b8
chalupa-bazooka <mteplyshev@gmail.com>
2024-05-28 12:30:21 +0400

    Add clarity & concistency
    - remove unncessary imports & context exports
    - add comments
    - move the effect to a separate utility file useElsewhereClick

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,27 @@
 // general
-import { useState, useEffect, useRef, createContext } from "react"
-import { useOutletContext } from "react-router-dom"
+import { useState, createContext } from "react"
 // components
 import InputWindow from "../components/Home/InputWindow"
 import OutputWindow from "../components/Home/OutputWindow"
 import AlternativeOptions from "../components/Home/AlternativeOptions"
-// data
-import { charsData } from "../data/charsData"
 
-export const TransliteratorContext = createContext()
+export const TransliteratorContext = createContext() // Context 2*
 
 export default function Home() {
 
-    const [latestOutput, setLatestOutput] = useState("")
-
-    const [activeAlternativeOption, setActiveAlternativeOption] = useState(
+    const [latestOutput, setLatestOutput] = useState("") // States 1.1*
+    const [activeAlternativeOption, setActiveAlternativeOption] = useState( //States 1.2*
         { shown: false, char: "", initLat: "", index: null }
     )
+    const [optionsDisplay, setOptionsDisplay] = useState(false) // States 1.3*
 
-
-    let letterOptionRef = useRef() // Effects 3.1*
-    useEffect(() => { // Effects 3.1*
-        let handler = (e) => {
-            if (letterOptionRef.current && !letterOptionRef.current.contains(e.target)) {
-                setActiveAlternativeOption({ shown: false, char: "", initLat: "", index: null })
-            }
-        }
-
-        document.addEventListener("mousedown", handler)
-
-        return () => {
-            document.removeEventListener("mousedown", handler)
-        }
-    })
-
-    const [optionsDisplay, setOptionsDisplay] = useState(false)
-    // this thing activates when a button (the only button so far)
-    // has been clicked in the Extra Options component. It's used
-    // to highlight characters.
-
-    const { language, vpWidth } = useOutletContext()
-
-    const transliteratorContextContent = {
+    const transliteratorContextContent = { // Context 2*
         activeAlternativeOption,
-        charsData,
-        language,
         latestOutput,
-        letterOptionRef,
         optionsDisplay,
         setActiveAlternativeOption,
         setLatestOutput,
         setOptionsDisplay,
-        vpWidth,
     }
 
 
@@ -78,4 +48,29 @@ RENDER STRUCTURE
 1.1. Input window
 1.2. Output window
 2. Alternative Options section
+
+
+COMMENTS
+1. States
+1.1. latestOutput
+Used in: OutputWindow, InputWindow
+Info: It's what gets displayed in the OutputWindow. We need it here to pass it to
+InputWindow for its "Clear" button funcitonality: upon clearing the InputWindow's
+contents it also clears the contents of the OutputWindow (with setLatestOutput).
+Since those 2 window components are siblings we can't pass the state between them,
+hence it's necessary to initialize it in their closest mutual parent.
+.
+1.2. activeAlternativeOption
+Used in: AlternativeOptions (& its children), InputWindow, OutputWindow
+Info: It displays the latest clicked highlighted character & its alternative
+options. Its setter is often used when it's necessary to dehighlight the character.
+.
+1.3. optionsDisplay
+Used in: AlternativeOptions (& its children), OutputWindow
+It activates character highlighting. Clicking a highlighted character makes its
+alternative options visible (see 1.2).
+.
+.
+2. Context
+All the 3 states & its setters declared here are passed to the context.
 */}
