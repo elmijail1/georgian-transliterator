3d55dee7a83bcb85a2e12293951ea6f8efe5c4ac
chalupa-bazooka <mteplyshev@gmail.com>
2024-06-09 11:55:10 +0400

    Add the cleaner effect & add digraphs

--- src/pages/MapOutputTest.jsx
+++ src/pages/MapOutputTest.jsx
@@ -1,12 +1,34 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { nanoid } from "nanoid"
 
 export default function MapOutputTest() {
+
+    {/*
+    to do
+    - I added digraphs and now things go crazy when you click a char.
+    The reason for that is that modifiedOutput's main reliance value is
+    index. Diphthongs break that. There are 2 ways to handle it:
+    ..- look for a way to make index work with digraphs (preferable);
+    ..- look for a way to make unique IDs work with them (might be a
+    good idea too).
+    I think I should try both approaches;
+    - add alternative options to the dictionary and see how it's going
+    to work with it;
+    - basically reproduce all the transliterator logic of the original
+    app (with a smaller dictionary of course) and see how it's going
+    to work;
+    - make the code slicker;
+    - transfer it to the real transliterator.     
+    */}
 
     const dictionary = [
         { lat: "a", arm: "ա" },
         { lat: "b", arm: "բ" },
         { lat: "c", arm: "ծ" },
+        { lat: "s", arm: "ս" },
+        { lat: "t", arm: "տ" },
+        { lat: "ts", arm: "ծ" },
+
     ]
 
     const [input, setInput] = useState()
@@ -23,7 +45,33 @@ export default function MapOutputTest() {
         const finalArray = []
         const inputArray = string.split("")
 
+        let prevChar = ""
+        const digraphs = {
+            total: ["t"],
+            sDigraphs: ["t"],
+        }
+
         inputArray.map((character, index) => {
+
+            console.log(character, prevChar)
+
+            if (prevChar.length > 1) {
+                prevChar = character
+                return
+            }
+
+            if (digraphs.total.includes(character)) {
+                if (
+                    digraphs.sDigraphs.includes(character)
+                    && inputArray[index+1] === "s"
+                ) {
+                    character = character + "s"
+                    prevChar = character
+                }
+            } else {
+                prevChar = character
+            }
+
             dictionary.map(entry => {
                 if (!modifiedOutput || !modifiedOutput.length) {
                     if (entry.lat === character) {
@@ -31,11 +79,7 @@ export default function MapOutputTest() {
                     }
                 } else {
 
-                    // THE DOUBLING PROBLEM IS SOMEWHERE HERE
-                    // KEEP DOING STEP-BY-STEPS TO FIGURE IT OUT
-
                     let found = 0
-                    // modifiedOutput.map((modChar) => {
                     modifiedOutput.map((modChar, modIndex) => {
                         if (found > 0) {
                             return
@@ -44,7 +88,7 @@ export default function MapOutputTest() {
                                 found++
                                 return finalArray.push({ lat: character, arm: modChar.arm, index: index, modified: true })
                             } else if (entry.lat === character) {
-                                if (modIndex === modifiedOutput.length-1) {
+                                if (modIndex === modifiedOutput.length - 1) {
                                     found++
                                     return finalArray.push({ lat: character, arm: entry.arm, index: index })
                                 } else {
@@ -57,7 +101,6 @@ export default function MapOutputTest() {
 
             })
         })
-        // console.log(finalArray)
         return finalArray
     }
 
@@ -93,7 +136,6 @@ export default function MapOutputTest() {
                     }
                 })
                 setModifiedOutput(newArray.filter(char => char.modified))
-                console.log(newArray)
                 return newArray
             }
         )
@@ -110,29 +152,6 @@ export default function MapOutputTest() {
                     {char.arm}
                 </span>
             )
-            // } else { // TRY MOVING THIS PART TO TRANSLITERATE
-            //     modifiedOutput.map(modChar => {
-            //         if (modChar.lat === char.lat && modChar.index === index) {
-            //             char = { ...char, arm: modChar.arm, modified: true }
-            //             return (
-            //                 <span
-            //                     key={nanoid()}
-            //                     onClick={() => changeCharsCase(index)}
-            //                 >
-            //                     {char.arm}
-            //                 </span>
-            //             )
-            //         }
-            //     })
-            //     return (
-            //         <span
-            //             key={nanoid()}
-            //             onClick={() => changeCharsCase(index)}
-            //         >
-            //             {char.arm}
-            //         </span>
-            //     )
-            // }
         })
     }
 
@@ -141,10 +160,16 @@ export default function MapOutputTest() {
         setOutput(transliterate(event.target.value))
     }
 
-    // WE ALSO NEED AN EFFECT THAT WILL CHECK THE LENGTH OF THE OUTPUT AND IF
-    // MODIFIED OUTPUT CONTAINS ITEMS WITH INDICES HIGHER THAN THE LENGTH OF OUTPUT,
-    // SUCH ITEMS SHOULD BE REMOVED (THAT WILL PREVENT RETYPING THE SAME TEXT &
-    // GETTING CHARS AUTO MODIFIED AGAIN )
+    // cleaner effect
+    useEffect(() => {
+        if (modifiedOutput) {
+            console.log(modifiedOutput)
+            setModifiedOutput((prevModifiedOutput) => {
+                return prevModifiedOutput.filter((modChar) => modChar.index < output.length)
+            })
+            console.log(modifiedOutput)
+        }
+    }, [output])
 
     return (
         <main>
@@ -165,3 +190,20 @@ export default function MapOutputTest() {
         </main>
     )
 }
+
+{/*
+COMMENTS
+
+CLEANER EFFECT
+Imagine a situation where you've typed in "aba" then clicked on the "b"
+and it became upper case ("aBa"). Then you delete it and type "cba".
+What you'll get is "cBa", although you haven't clicked that "b" after
+typing "cba". The reason for that is the modifiedOutput state mechanism
+that relies on the index of the output characters. So we need to remove
+all the "overflow" values from that state array when we remove parts
+of our input (or all of it). Hence after any output change we check if
+the modifiedOutput state array contains any values that have index of
+a greater value than the length of the output. All such values are
+filtered and we don't get auto change in cases similar to the one
+described above.
+*/}
