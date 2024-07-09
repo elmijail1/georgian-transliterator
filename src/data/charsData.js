a7747692a5b645ee6039db6774070a3c420bf3d4
chalupa-bazooka <mteplyshev@gmail.com>
2024-07-09 18:12:09 +0400

    Fix tsh's options

--- src/data/charsData.js
+++ src/data/charsData.js
@@ -1,155 +1,155 @@
 export const charsData = [
-    {
-        lat: "a",
-        geo: "ა",
-    },
-    {
-        lat: "b",
-        geo: "ბ",
-    },
-    {
-        lat: "c",
-        geo: "ც",
-        options: ["წ", "ც"],
-    },
-    {
-        lat: "ch",
-        geo: "ჩ",
-        options: ["ჭ", "ჩ"],
-    },
-    {
-        lat: "d",
-        geo: "დ",
-    },
-    {
-        lat: "dz",
-        geo: "ძ",
-    },
-    {
-        lat: "e",
-        geo: "ე",
-    },
-    {
-        lat: "f",
-        geo: "ფ"
-    },
-    {
-        lat: "g",
-        geo: "გ",
-        options: ["ღ", "გ"],
-    },
-    {
-        lat: "gh",
-        geo: "ღ",
-    },
-    {
-        lat: "h",
-        geo: "ჰ",
-        options: ["ხ", "ჰ"],
-    },
-    {
-        lat: "i",
-        geo: "ი",
-    },
-    {
-        lat: "j",
-        geo: "ჯ",
-        options: ["ჟ", "ჯ"],
-    },
-    {
-        lat: "k",
-        geo: "კ",
-        options: ["ქ", "ყ", "კ"],
-    },
-    {
-        lat: "kh",
-        geo: "ხ",
-    },
-    {
-        lat: "l",
-        geo: "ლ",
-    },
-    {
-        lat: "m",
-        geo: "მ",
-    },
-    {
-        lat: "n",
-        geo: "ნ",
-    },
-    {
-        lat: "o",
-        geo: "ო",
-    },
-    {
-        lat: "p",
-        geo: "პ",
-        options: ["ფ", "პ"],
-    },
-    {
-        lat: "ph",
-        geo: "ფ",
-    },
-    {
-        lat: "q",
-        geo: "ქ",
-    },
-    {
-        lat: "r",
-        geo: "რ",
-    },
-    {
-        lat: "s",
-        geo: "ს",
-    },
-    {
-        lat: "sh",
-        geo: "შ",
-    },
-    {
-        lat: "t",
-        geo: "თ",
-        options: ["ტ", "თ"],
-    },
-    {
-        lat: "ts",
-        geo: "ც",
-        options: ["წ", "ც", "თს", "ტს"],
-    },
-    {
-        lat: "tsh",
-        geo: "თშ",
-        options: ["ტშ"],
-    },
-    {
-        lat: "u",
-        geo: "უ",
-    },
-    {
-        lat: "v",
-        geo: "ვ",
-    },
-    {
-        lat: "w",
-        geo: "წ",
-        options: ["წ", "უ"],
-    },
-    {
-        lat: "x",
-        geo: "ხ",
-        options: ["ხ", "კს"],
-    },
-    {
-        lat: "y",
-        geo: "ყ",
-        options: ["ი", "ყ"],
-    },
-    {
-        lat: "z",
-        geo: "ზ",
-    },
-    {
-        lat: "zh",
-        geo: "ჟ",
-    },
-]
+  {
+    lat: "a",
+    geo: "ა",
+  },
+  {
+    lat: "b",
+    geo: "ბ",
+  },
+  {
+    lat: "c",
+    geo: "ც",
+    options: ["წ", "ც"],
+  },
+  {
+    lat: "ch",
+    geo: "ჩ",
+    options: ["ჭ", "ჩ"],
+  },
+  {
+    lat: "d",
+    geo: "დ",
+  },
+  {
+    lat: "dz",
+    geo: "ძ",
+  },
+  {
+    lat: "e",
+    geo: "ე",
+  },
+  {
+    lat: "f",
+    geo: "ფ",
+  },
+  {
+    lat: "g",
+    geo: "გ",
+    options: ["ღ", "გ"],
+  },
+  {
+    lat: "gh",
+    geo: "ღ",
+  },
+  {
+    lat: "h",
+    geo: "ჰ",
+    options: ["ხ", "ჰ"],
+  },
+  {
+    lat: "i",
+    geo: "ი",
+  },
+  {
+    lat: "j",
+    geo: "ჯ",
+    options: ["ჟ", "ჯ"],
+  },
+  {
+    lat: "k",
+    geo: "კ",
+    options: ["ქ", "ყ", "კ"],
+  },
+  {
+    lat: "kh",
+    geo: "ხ",
+  },
+  {
+    lat: "l",
+    geo: "ლ",
+  },
+  {
+    lat: "m",
+    geo: "მ",
+  },
+  {
+    lat: "n",
+    geo: "ნ",
+  },
+  {
+    lat: "o",
+    geo: "ო",
+  },
+  {
+    lat: "p",
+    geo: "პ",
+    options: ["ფ", "პ"],
+  },
+  {
+    lat: "ph",
+    geo: "ფ",
+  },
+  {
+    lat: "q",
+    geo: "ქ",
+  },
+  {
+    lat: "r",
+    geo: "რ",
+  },
+  {
+    lat: "s",
+    geo: "ს",
+  },
+  {
+    lat: "sh",
+    geo: "შ",
+  },
+  {
+    lat: "t",
+    geo: "თ",
+    options: ["ტ", "თ"],
+  },
+  {
+    lat: "ts",
+    geo: "ც",
+    options: ["წ", "ც", "თს", "ტს"],
+  },
+  {
+    lat: "tsh",
+    geo: "თშ",
+    options: ["თშ", "ტშ"],
+  },
+  {
+    lat: "u",
+    geo: "უ",
+  },
+  {
+    lat: "v",
+    geo: "ვ",
+  },
+  {
+    lat: "w",
+    geo: "წ",
+    options: ["წ", "უ"],
+  },
+  {
+    lat: "x",
+    geo: "ხ",
+    options: ["ხ", "კს"],
+  },
+  {
+    lat: "y",
+    geo: "ყ",
+    options: ["ი", "ყ"],
+  },
+  {
+    lat: "z",
+    geo: "ზ",
+  },
+  {
+    lat: "zh",
+    geo: "ჟ",
+  },
+];
